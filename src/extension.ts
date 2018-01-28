8d4f5c062892a560279a835904dbf2e3a042dbde
Kevin Cunnane <kevcunnane@gmail.com>
2018-01-28 08:24:35 -0800

    Test to check whether node is added on connecting to HDFS

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { VscodeWrapper } from './vscodeWrapper';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,19 +10,34 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "hdfsbrowser" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+
+export class HdfsProvider implements vscode.TreeDataProvider<HdfsNode> {
+    onDidChangeTreeData?: vscode.Event<HdfsNode>;
+
+    constructor(context: vscode.ExtensionContext, vscodeApi: VscodeWrapper) {
+
+    }
+
+    getTreeItem(element: HdfsNode): vscode.TreeItem | Thenable<vscode.TreeItem> {
+        throw new Error("Method not implemented.");
+    }
+
+    getChildren(element?: HdfsNode): vscode.ProviderResult<HdfsNode[]> {
+        throw new Error("Method not implemented.");
+    }
+
+    addConnection(path: string): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
+
+export class HdfsNode {
+
 }
